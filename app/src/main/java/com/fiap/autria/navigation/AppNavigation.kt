ba7e02b2815550e5c573c2e070baa701422e56eb
macarrilho09 <ma.carrilho09@gmail.com>
2026-08-23 00:06:24 -0300

    assistente de ia e sobre funcionando

--- app/src/main/java/com/fiap/autria/navigation/AppNavigation.kt
+++ app/src/main/java/com/fiap/autria/navigation/AppNavigation.kt
@@ -8,11 +8,6 @@ import com.fiap.autria.ui.screens.home.HomeScreen
 import com.fiap.autria.ui.screens.ia.IaScreen
 import com.fiap.autria.ui.screens.settings.SettingScreen
 import com.fiap.autria.ui.screens.about.AboutScreen
-import com.fiap.autria.ui.theme.AutriaTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 object Routes {
     const val HOME = "home"
@@ -22,52 +17,49 @@ object Routes {
 }
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(
+    onToggleTheme: () -> Unit
+) {
     val navController = rememberNavController()
-    var isDarkTheme by remember { mutableStateOf(false) }
 
-    AutriaTheme(darkTheme = isDarkTheme) {
-        NavHost(
-            navController = navController,
-            startDestination = Routes.HOME
-        ) {
-            composable(Routes.HOME) {
-                HomeScreen(
-                    onSettingsClick = {
-                        navController.navigate(Routes.SETTINGS)
-                    },
-                    onIaClick = {
-                        navController.navigate(Routes.IA)
-                    }
-                )
-            }
-            composable(Routes.SETTINGS) {
-                SettingScreen(
-                    onBackClick = {
-                        navController.popBackStack()
-                    },
-                    onToggleTheme = {
-                        isDarkTheme = !isDarkTheme
-                    },
-                    onAboutClick = {
-                        navController.navigate(Routes.ABOUT)
-                    }
-                )
-            }
-            composable(Routes.ABOUT) {
-                AboutScreen(
-                    onBackClick = {
-                        navController.popBackStack()
-                    }
-                )
-            }
-            composable(Routes.IA) {
-                IaScreen(
-                    onBackClick = {
-                        navController.popBackStack()
-                    }
-                )
-            }
+    NavHost(
+        navController = navController,
+        startDestination = Routes.HOME
+    ) {
+        composable(Routes.HOME) {
+            HomeScreen(
+                onSettingsClick = {
+                    navController.navigate(Routes.SETTINGS)
+                },
+                onIaClick = {
+                    navController.navigate(Routes.IA)
+                }
+            )
+        }
+        composable(Routes.SETTINGS) {
+            SettingScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                },
+                onToggleTheme = onToggleTheme,
+                onAboutClick = {
+                    navController.navigate(Routes.ABOUT)
+                }
+            )
+        }
+        composable(Routes.ABOUT) {
+            AboutScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+        composable(Routes.IA) {
+            IaScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
         }
     }
 }
